d77ee428d8833867e877fa8c7ceb0ed5c1d77682
DanielQuick <danielquick14@gmail.com>
2023-12-21 10:52:16 -0600

    Update android colors

--- android/src/main/kotlin/com/danielquick/flutter_inbrain/FlutterInbrainPlugin.kt
+++ android/src/main/kotlin/com/danielquick/flutter_inbrain/FlutterInbrainPlugin.kt
@@ -2,13 +2,13 @@ package com.danielquick.flutter_inbrain
 
 import FlutterError
 import InbrainApi
-import android.R
 import android.app.Activity
 import android.graphics.Color
 import androidx.annotation.NonNull
 import com.inbrain.sdk.InBrain
 import com.inbrain.sdk.callback.StartSurveysCallback
 import com.inbrain.sdk.callback.SurveysAvailableCallback
+import com.inbrain.sdk.config.StatusBarConfig
 import com.inbrain.sdk.config.ToolBarConfig
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.embedding.engine.plugins.activity.ActivityAware
@@ -47,13 +47,18 @@ class FlutterInbrainPlugin: FlutterPlugin, InbrainApi, ActivityAware {
   override fun initialize(clientId: String, secret: String, isS2S: Boolean, userId: String) {
     InBrain.getInstance().setInBrain(activity, clientId, secret, isS2S, userId);
     val toolBarConfig = ToolBarConfig()
+    val statusBarConfig = StatusBarConfig()
+    statusBarConfig.statusBarColorResId = R.color.inbrain_color
+    statusBarConfig.isStatusBarIconsLight = false
     toolBarConfig.title = "Answer2Earn" // set title
 
+    toolBarConfig.setToolbarColorResId(R.color.inbrain_color) // set toolbar color with status bar
+      .setBackButtonColorResId(android.R.color.white) // set icon color
+      .setTitleColorResId(android.R.color.white); //  set toolbar text
 
-    toolBarConfig.setToolbarColor(Color.parseColor("x8bd753"))
-        .setBackButtonColor(Color.parseColor("xffffff"))
     toolBarConfig.setElevationEnabled(false)
     InBrain.getInstance().setToolbarConfig(toolBarConfig)
+    InBrain.getInstance().setStatusBarConfig(statusBarConfig)
   }
 
   override fun showSurveys(callback: (Result<Unit>) -> Unit) {
